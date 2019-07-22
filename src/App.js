f148a4cac404421a23d0cd49bb9dee3a9ae82f09
Simon Phillips <simon.phillips01@icloud.com>
2019-07-22 11:32:20 +0100

    Added styled components, linked image and changed tiles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,189 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import styled from 'styled-components';
 
-function App() {
+// This example shows how to render two different screens
+// (or the same screen in a different context) at the same url,
+// depending on how you got there.
+//
+// Click the colors and see them full screen, then "visit the
+// gallery" and click on the colors. Note the URL and the component
+// are the same as before but now we see them inside a modal
+// on top of the old screen.
+
+class ModalSwitch extends Component {
+  // We can pass a location to <Switch/> that will tell it to
+  // ignore the router's current location and use the location
+  // prop instead.
+  //
+  // We can also use "location state" to tell the app the user
+  // wants to go to `/img/2` in a modal, rather than as the
+  // main page, keeping the gallery visible behind it.
+  //
+  // Normally, `/img/2` wouldn't match the gallery at `/`.
+  // So, to get both screens to render, we can save the old
+  // location and pass it to Switch, so it will think the location
+  // is still `/` even though its `/img/2`.
+  previousLocation = this.props.location;
+
+  componentWillUpdate(nextProps) {
+    let { location } = this.props;
+
+    // set previousLocation if props.location is not modal
+    if (
+      nextProps.history.action !== "POP" &&
+      (!location.state || !location.state.modal)
+    ) {
+      this.previousLocation = this.props.location;
+    }
+  }
+
+  render() {
+    let { location } = this.props;
+
+    let isModal = !!(
+      location.state &&
+      location.state.modal &&
+      this.previousLocation !== location
+    ); // not initial render
+
+    return (
+      <div>
+        <Switch location={isModal ? this.previousLocation : location}>
+          <Route exact path="/" component={Home} />
+          <Route path="/gallery" component={Gallery} />
+          <Route path="/img/:id" component={ImageView} />
+        </Switch>
+        {isModal ? <Route path="/img/:id" component={Modal} /> : null}
+      </div>
+    );
+  }
+}
+
+const Image = styled.div`
+  width: 400px;
+  height: 400px;
+  background: no-repeat center/150% url(/img/${({index}) => index}.jpg);
+`
+
+const IMAGES = [
+  { id: 1, title: "Plants window", },
+  { id: 2, title: "Big plant", },
+  { id: 3, title: "plant shop",},
+  { id: 4, title: "plant bookcase", },
+  { id: 5, title: "up close plant",},
+  { id: 6, title: "plant imac", },
+  { id: 7, title: "plant cafe", },
+  { id: 8, title: "plant pots",},
+  { id: 9, title: "small plants", },
+  { id: 10, title: "tiny plants",},
+  { id: 11, title: "classical plants", },
+  { id: 12, title: "plant presentation", },
+  { id: 13, title: "plant spec",},
+  { id: 14, title: "up close plant 2", },
+  { id: 15, title: "plant shop 2",}
+];
+
+function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Link to="/gallery">Visit the Gallery</Link>
+      <h2>Featured Images</h2>
+      <ul>
+        <li>
+          <Link to="/img/2">Plants</Link>
+        </li>
+        <li>
+          <Link to="/img/4">plants</Link>
+        </li>
+      </ul>
     </div>
   );
 }
 
-export default App;
+function Gallery() {
+  return (
+    <div>
+      {IMAGES.map(i => (
+        <Link
+          key={i.id}
+          to={{
+            pathname: `/img/${i.id}`,
+            // this is the trick!
+            state: { modal: true }
+          }}
+        >
+          <Image index={i.id} />
+          <p>{i.title}</p>
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+function ImageView({ match }) {
+  let image = IMAGES[parseInt(match.params.id, 10) -1];
+
+  if (!image) return <div>Image not found</div>;
+
+  return (
+    <div>
+      <h1>{image.title}</h1>
+      <Image index={image.id} />
+    </div>
+  );
+}
+
+function Modal({ match, history }) {
+  let image = IMAGES[parseInt(match.params.id, 10) -1];
+
+  if (!image) return null;
+
+  let back = e => {
+    e.stopPropagation();
+    history.goBack();
+  };
+
+  return (
+    <div
+      onClick={back}
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        background: "rgba(0, 0, 0, 0.15)"
+      }}
+    >
+      <div
+        className="modal"
+        style={{
+          position: "absolute",
+          background: "#fff",
+          top: 25,
+          left: "10%",
+          right: "10%",
+          padding: 15,
+          border: "2px solid #444"
+        }}
+      >
+        <h1>{image.title}</h1>
+        <Image index={image.id} />
+        <button type="button" onClick={back}>
+          Close
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function ModalGallery() {
+  return (
+    <Router>
+      <Route component={ModalSwitch} />
+    </Router>
+  );
+}
+
+export default ModalGallery;
